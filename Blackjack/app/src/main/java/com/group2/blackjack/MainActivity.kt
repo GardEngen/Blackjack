9ad9be5baf9ab4958d5226189b6ed982f3f39457
GardEngen <gardengen@gmail.com>
2017-10-20 20:12:43 +0200

    få det på

--- Blackjack/app/src/main/java/com/group2/blackjack/MainActivity.kt
+++ Blackjack/app/src/main/java/com/group2/blackjack/MainActivity.kt
@@ -7,16 +7,23 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var splitButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val splitButton = findViewById(R.id.splitButton)
+        splitButton = findViewById(R.id.splitButton) as Button
 
 
+
+        buttonAction();
+
+    }
+
+    private fun buttonAction() {
         splitButton.setOnClickListener{
             println("hei")
         }
-
     }
 }
