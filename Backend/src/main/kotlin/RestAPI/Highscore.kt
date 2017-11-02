674294adf8196802e74894df7874afb5f1b29132
GardEngen <gardengen@gmail.com>
2017-11-02 19:03:38 +0100

    added backend

---
+++ Backend/src/main/kotlin/RestAPI/Highscore.kt
@@ -0,0 +1,27 @@
+package RestAPI
+import com.google.gson.Gson
+import Entities.Highscore
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.main() {
+    val gson = Gson()
+
+
+
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            val doktor = Highscore("Test")
+            val json = gson.toJson(doktor)
+            call.respondText(json, ContentType.Application.Json)
+        }
+    }
+}
