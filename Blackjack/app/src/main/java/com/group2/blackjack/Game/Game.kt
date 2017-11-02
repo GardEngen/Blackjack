ba1cb7cce13cc2c5a8b300290331ccbdb5ffc11e
NikolaiGrieg <raugzok@gmail.com>
2017-11-02 16:48:07 +0100

    fixed bugs in gamelogic

--- Blackjack/app/src/main/java/com/group2/blackjack/Game/Game.kt
+++ Blackjack/app/src/main/java/com/group2/blackjack/Game/Game.kt
@@ -1,6 +1,7 @@
 package com.group2.blackjack.Game
 
 import android.content.Context
+import android.widget.TextView
 import com.group2.blackjack.Entities.Card
 import com.group2.blackjack.Entities.Deck
 import com.group2.blackjack.Entities.Table
@@ -8,7 +9,8 @@ import com.group2.blackjack.Entities.Table
 /**
  * Created by raugz on 11/2/2017.
  */
-class Game {
+class Game constructor(tv : TextView){
+    var balanceText = tv
     lateinit var rules : CardRules
     lateinit var table : Table
     lateinit var deck : Deck
@@ -22,7 +24,7 @@ class Game {
         deck.reShuffle()
 
         //init hands 2 cards each
-        for(i in 0..4){ // should do 0 to 3, might draw 5 if 4
+        for(i in 0..3){ // draws 0 to 3, 4 cards
             if (i%2 == 0){
                 table.dealCard(true, deck.draw())
             }
@@ -34,6 +36,7 @@ class Game {
         //TODO add entry money input from user
         val bet = 20
         table.placeBet(bet)
+        balanceText.text = table.money.toString()
     }
 
     private fun initGame(){
@@ -48,7 +51,7 @@ class Game {
             table.addMoney(bet*2)
         }
         table.flushHands()
-        Thread.sleep(2000) // allow user to see result
+        //Thread.sleep(2000) // allow user to see result
         startRound()
     }
 
@@ -64,19 +67,29 @@ class Game {
     }
 
     fun hit(){
+        //println("hittttttt")
         table.dealCard(true, deck.draw())
         if (checkOver(table.player, table.dealer)){ // true = someone has over 21 TODO fix real rules
             endRound(rules.getWinner(table.player, table.dealer)) // true = player won
         }
-        else{
-            table.dealCard(false, deck.draw())
+        else{ // dealers turn
+            if(rules.getScore(table.dealer) < 17){
+                table.dealCard(false, deck.draw())
+            }
         }
     }
 
     fun stand(){
-        table.dealCard(false, deck.draw())
         if (checkOver(table.player, table.dealer)){ // true = someone has over 21 TODO fix real rules
             endRound(rules.getWinner(table.player, table.dealer)) // true = player won
+        }
+        else{
+            val lessThanPlayer = rules.getScore(table.dealer) < rules.getScore(table.player)
+            val under21 = rules.getScore(table.dealer) < 21
+            while(lessThanPlayer || under21){
+                table.dealCard(false, deck.draw())
+            }
+            endRound(rules.getWinner(table.player, table.dealer))
         }
     }
     //TODO future
