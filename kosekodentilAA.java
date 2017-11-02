809b1c3e30d86fadc5da3f129f5eac7748db9c6d
GardEngen <gardengen@gmail.com>
2017-11-02 12:06:30 +0100

    player

--- kosekodentilAA.java
+++ kosekodentilAA.java
@@ -90,3 +90,25 @@ class Deck constructor(){
 enum class Color {
     SPADES, DIAMONDS, HEARTHS, CLUBS
 }
+
+
+
+/**
+ * Created by Åsmund Hammer on 19.10.17.
+ */
+class Player constructor(name: String, wallet: Int){
+    val name = name
+    var wallet = wallet
+    var cards = ArrayList<Card>()
+
+    fun collect(card: Card){
+        cards.add(card)
+    }
+
+    fun ditchHand(){
+        cards.clear()
+    }
+
+
+
+}
